fc7da8946641cc53b5844de3f757eeb5a4255848
Diego Veiga <sdiegoveiga@gmail.com>
2024-10-28 21:30:42 -0300

    Create validation to user with same email

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { compare, hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 
@@ -9,13 +10,29 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     password: z.string().min(6),
   })
 
+  // $2a$06$d19ZiT9qRctzgMiPU3ldnOKWw/xjwWIeSlQZdFImJjLw1BkLO6kRC
+
   const { email, name, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    reply.status(409).send({
+      error: 'User with this email already exists',
+    })
+  }
 
   await prisma.user.create({
     data: {
       email,
       name,
-      password_hash: password,
+      password_hash,
     },
   })
 
