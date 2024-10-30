7c92cb107d4c4d0fb3682df1c828f0d605ece152
Diego Veiga <sdiegoveiga@gmail.com>
2024-10-30 08:04:23 -0300

    Creating error class

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
@@ -21,8 +22,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       name,
       password,
     })
-  } catch (error) {
-    reply.status(409).send()
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    return reply.status(500).send() // todo: fix
   }
 
   return reply.status(201).send()
