33167e93a241ec49a985cf89e1ab8e1c08741be0
Diego Veiga <sdiegoveiga@gmail.com>
2024-11-02 11:37:41 -0300

    TDD and Mocking in check-in tests

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,5 +1,6 @@
+import { afterEach } from 'node:test'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { CheckInUseCase } from './check-in'
 
 let checkInsRepository: InMemoryCheckInsRepository
@@ -9,9 +10,55 @@ describe('CheckIn use case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    console.log(checkIn.created_at)
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  // TDD -> red (Erro no teste) -> green (passar no teste), refactor (refatorar)
+
+  it('should not be able to check in twice the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-id',
+        userId: 'user-id',
+      })
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
     const { checkIn } = await sut.execute({
       gymId: 'gym-id',
       userId: 'user-id',
