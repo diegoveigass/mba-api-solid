d293c71c1e23eb504e33e01dbc0533ac84f8b028
Diego Veiga <sdiegoveiga@gmail.com>
2024-11-03 21:01:42 -0300

    Creating prisma gyms repository

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,5 +1,5 @@
-import { CheckIn, Prisma } from '@prisma/client'
-import { CheckInsRepository } from '../check-ins-repository'
+import type { CheckIn, Prisma } from '@prisma/client'
+import type { CheckInsRepository } from '../check-ins-repository'
 import { prisma } from '@/lib/prisma'
 import dayjs from 'dayjs'
 
@@ -13,6 +13,7 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
 
     return checkIn
   }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = prisma.checkIn.create({
       data,
@@ -20,6 +21,7 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
 
     return checkIn
   }
+
   async save(data: CheckIn) {
     const checkIn = await prisma.checkIn.update({
       where: {
@@ -30,6 +32,7 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
 
     return checkIn
   }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date')
     const endOfTheDay = dayjs(date).endOf('date')
@@ -46,6 +49,7 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
 
     return checkIn
   }
+
   async findManyByUserId(userId: string, page: number) {
     const checkIns = await prisma.checkIn.findMany({
       where: {
@@ -57,6 +61,7 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
 
     return checkIns
   }
+
   async countByUserId(userId: string) {
     const count = await prisma.checkIn.count({
       where: {
