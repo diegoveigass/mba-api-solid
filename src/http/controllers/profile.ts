e6b3410e5dcacb1b6c93e606a8766a0cbe63dcbb
Diego Veiga <sdiegoveiga@gmail.com>
2024-11-04 12:29:05 -0300

    Creating profile controller and verify jwt middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,23 +1,17 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
-  // try {
-  //   const authenticateUseCase = makeAuthenticateUseCase()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  //   await authenticateUseCase.execute({
-  //     email,
-  //     password,
-  //   })
-  // } catch (err) {
-  //   if (err instanceof InvalidCredentialsError) {
-  //     return reply.status(400).send({ message: err.message })
-  //   }
-
-  //   throw err
-  // }
-
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
