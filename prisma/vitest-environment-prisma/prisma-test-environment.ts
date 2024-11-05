d6b9209dcd9b4a12f1068120a6e5b97f5f1afd23
Diego Veiga <sdiegoveiga@gmail.com>
2024-11-05 17:13:58 -0300

    Creating controller tests (authenticate, register and profile)

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -30,8 +30,6 @@ export default (<Environment>{
 
     execSync('npx prisma migrate deploy')
 
-    console.log(generateDatabaseURL(schema))
-
     return {
       async teardown() {
         await prisma.$executeRawUnsafe(
